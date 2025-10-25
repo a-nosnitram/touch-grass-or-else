76a7dd068090b6984fae0114c9f80daf01b07c17
Fedor Lebedev <flebedev777@gmail.com>
2025-10-25 15:56:05 +0100

    fix

--- ui/health_bar.py
+++ ui/health_bar.py
@@ -9,12 +9,15 @@ from pynput.mouse import Button, Controller
 import ctypes
 
 class CameraWidget(QWidget):
-    def __init__(self):
+    def __init__(self, main_window):
         super().__init__()
         uic.loadUi('data/camera_alert.ui', self)
+        self.setWindowIcon(QIcon('data/grass.png'))
         self.setWindowTitle("GRASS ALERT")
         self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         self.grabMouse()
+
+        self.main_window = main_window
 
         self.video_label = self.findChild(QLabel, "cameraLabel")
         screen_geometry = QApplication.primaryScreen().geometry()
@@ -34,10 +37,16 @@ class CameraWidget(QWidget):
 
         ctypes.windll.user32.BlockInput(True)
         self.timer = QTimer()
+        self.progressBarTimer = QTimer()
         self.timer.timeout.connect(self.update_frame)
-        self.timer.timeout.connect(self.update_progress_bar)
-        self.timer.start(500)
+        self.progressBarTimer.timeout.connect(self.update_progress_bar)
+        self.timer.start(30)
+        self.progressBarTimer.start(100)
 
+    def reset(self):
+        self.value = 0
+        self.timer.start()
+        self.progressBarTimer.start()
 
     def update_progress_bar(self):
         self.value += 1
@@ -45,10 +54,12 @@ class CameraWidget(QWidget):
 
         if self.value >= 100:
             self.timer.stop()
+            self.progressBarTimer.stop()
             self.hide()
             ctypes.windll.user32.BlockInput(False)
-            self.MainWindow.showFullScreen()
             self.releaseMouse()
+            self.main_window.show()
+            self.main_window.reset()
 
 
     def update_frame(self):
@@ -67,9 +78,9 @@ class CameraWidget(QWidget):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Touch Grass")
-        self.setWindowIcon(QIcon("grass.png"))
         uic.loadUi('data/health_bar.ui', self)
+        self.setWindowTitle("Touch Grass")
+        self.setWindowIcon(QIcon("data/grass.png"))
 
         self.progress = self.findChild(QProgressBar, "progressBar")
         self.progress.setMinimum(0)
@@ -79,9 +90,14 @@ class MainWindow(QMainWindow):
         self.timer = QTimer()
         self.timer.timeout.connect(self.update_progress)
         self.value = 100
-        self.timer.start(10)
+        self.timer.start(100)
 
-        self.camera_widget = CameraWidget()
+        self.camera_widget = CameraWidget(self)
+
+    def reset(self):
+        self.value = 100
+        self.progress.setValue(self.value)
+        self.timer.start(100)
 
     def update_progress(self):
         self.value -= 1
@@ -90,6 +106,7 @@ class MainWindow(QMainWindow):
         if self.value <= 0:
             self.timer.stop()
             self.hide()
+            self.camera_widget.reset()
             self.camera_widget.showFullScreen()
 
 
